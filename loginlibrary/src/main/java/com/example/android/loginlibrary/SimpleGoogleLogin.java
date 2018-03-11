15c814ef4b821e30c91224051f586f52a5eea463
ritik1991998 <ritikkne@gmail.com>
2018-03-11 16:22:48 +0530

    google logging library completed

--- loginlibrary/src/main/java/com/example/android/loginlibrary/SimpleGoogleLogin.java
+++ loginlibrary/src/main/java/com/example/android/loginlibrary/SimpleGoogleLogin.java
@@ -2,12 +2,8 @@ package com.example.android.loginlibrary;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -21,19 +17,22 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.auth.UserProfileChangeRequest;
 
 /**
  * Created by ritik on 10-03-2018.
  */
 
-public class GoogleLogin {
-    private static FirebaseUser user;
+public class SimpleGoogleLogin {
     private static FirebaseAuth mAuth;
     private GoogleSignInClient mGoogleSignInClient;
-    private final static int RC_SIGN_IN = 1;
+    private int resultCodeSignIn;
+    private Activity activity;
+    private String googleToken;
 
-    public GoogleLogin() {
+    public SimpleGoogleLogin(Activity loginActivity, int resultCodeSignIn, String googleToken) {
+        this.activity = loginActivity;
+        this.resultCodeSignIn = resultCodeSignIn;
+        this.googleToken = googleToken;
     }
 
     private OnGoogleLoginResult mOnGoogleLoginResult;
@@ -42,73 +41,77 @@ public class GoogleLogin {
         public void resultSuccessful(FirebaseUser registeredUser);
 
         public void resultError(Exception errorResult);
-
-        public void resultName(FirebaseUser registeredUser);
-
-        public void resultDp(Uri uploadUriLink);
-
-        public void wrongCrudentials(String errorMessage);
     }
 
     public void setOnGoogleLoginResult(OnGoogleLoginResult eventListener) {
         mOnGoogleLoginResult = eventListener;
-
     }
 
-    public void attemptGoogleLogin(@NonNull Activity var1, String email, String passwordinput, String passwordRecheck, final String userName, final Uri uploadedDpLink) {
+    public void attemptGoogleLogin() {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestProfile()
+                .requestIdToken(googleToken)
                 .requestEmail()
                 .build();
 
-        mGoogleSignInClient = GoogleSignIn.getClient(var1, gso);
-
+        mGoogleSignInClient = GoogleSignIn.getClient(activity, gso);
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        var1.startActivityForResult(signInIntent, RC_SIGN_IN);
+        activity.startActivityForResult(signInIntent, resultCodeSignIn);
+
+        Log.i("point 64", "reached");
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
-
+        Log.i("point 88", (resultCode == activity.RESULT_OK) + "");
+        Log.i("point 83", "activity result");
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
+        if (requestCode == resultCodeSignIn) {
+
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 // Google Sign In was successful, authenticate with Firebase
+                Log.i("point 75", "activity result");
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account);
             } catch (ApiException e) {
                 // Google Sign In failed, update UI appropriately
-                Log.i("", "Google sign in failed", e);
-                // ...
+                e.printStackTrace();
+                if (mOnGoogleLoginResult != null) {
+                    mOnGoogleLoginResult.resultError(e);
+                }
             }
+
+        } else {
+            Log.i("point 83", "signInWithCredential:failure");
         }
+
     }
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-        Log.i("point 566", "firebaseAuthWithGoogle:" + account.getId());
+        Log.i("point 92", "firebaseAuthWithGoogle:" + account.getId());
+        mAuth = FirebaseAuth.getInstance();
 
         AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
         mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.i("signInWithCrential:suce", "point 575");
+                            Log.i("signInWithCrential:suce", "point 101");
                             FirebaseUser user = mAuth.getCurrentUser();
 
-                            Intent intent = new Intent(getApplicationContext(), com.example.android.loginaction.MainActivity.class);
-                            intent.putExtra("result", 1);
-                            setResult(Activity.RESULT_OK, intent);
-                            startActivity(intent);
-                            Toast.makeText(LoginActivity.this, "logged in", Toast.LENGTH_SHORT).show();
+                            if (mOnGoogleLoginResult != null) {
+                                mOnGoogleLoginResult.resultSuccessful(user);
+                            }
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.i("point 580", "signInWithCredential:failure", task.getException());
-
+                            Log.i("point 109", "signInWithCredential:failure", task.getException());
+                            if (mOnGoogleLoginResult != null) {
+                                mOnGoogleLoginResult.resultError(task.getException());
+                            }
                         }
+                        Log.i("reached", "point 114");
 
                     }
                 });
